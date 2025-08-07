53f58523d3cd6f78a425886493ac5df41da235fe
LAWAGGG <ahmadfagih.arrifai@gmail.com>
2025-08-07 13:50:25 +0700

    mengurangi baris kode

--- index.php
+++ index.php
@@ -5,18 +5,3 @@ $user = new User("localhost", "root", "", "authoop");
 
 $user->update("2","gwehhhh", "gweh", "gweh");
 var_dump($user->get());
-
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
-echo("kode ini sudah dimodifikasi");
